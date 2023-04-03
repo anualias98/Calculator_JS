0a3ebe9282d711b35fd2a1da0595cad6207eb68e
anualias98 <122264818+anualias98@users.noreply.github.com>
2023-04-03 22:10:43 +0530

    first commit

---
+++ JS/sample.js
@@ -0,0 +1,10 @@
+//var person="Anu"
+//let person1="Minu"
+//document.write(person)
+//document.write("<br>");
+//document.write(typeof(person))
+let x = myFunction(4, 3);   // Function is called, return value will end up in x
+
+function myFunction(a, b) {
+  return a * b;             // Function returns the product of a and b
+}
